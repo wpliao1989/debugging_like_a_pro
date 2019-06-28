06e75c4b9a7cee66ea8a6c1d2c3df9cefb6b98d0
Wei-Ping Liao <wei-ping.liao@appfolio.com>
2019-06-28 15:19:38 -0700

    Add instructions to the challenge problem

--- ruby/challenge.rb
+++ ruby/challenge.rb
@@ -1,6 +1,20 @@
+#####################################################################
+# Challenge problem:
+#
+# - Please fix the app and the page object
+#   so that it prints out "You did it!"
+#
+# - The first thing you have to fix is the `path`
+#   in the page object
+#
+# - The second thing is there's a bug in the mini rack app
+#   could you find it and fix it?
+#####################################################################
+
 require 'bundler/setup'
 require 'ae_page_objects'
 
+# Mini rack app
 Capybara.app = Proc.new do |env|
   if env['PATH_INFO'].split('/')[-1] == 'success'
     ['200', { 'Content-Type' => 'text/html' }, ['<h1>You did it!</h1>']]
@@ -9,6 +23,7 @@ Capybara.app = Proc.new do |env|
   end
 end
 
+# Page object
 class LoginPage < AePageObjects::Document
   path :fix_me
 end
